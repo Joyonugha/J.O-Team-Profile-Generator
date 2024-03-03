7e83c782cb033f19ea9027e5ff726dce75618169
Joy Onugha <joyonugha@gmail.com>
2024-03-03 16:12:06 +0000

    added the team manager's information

--- index.js
+++ index.js
@@ -10,17 +10,68 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-// This Function writes the HTML to a file
+
+// This Function writes the HTML to a file (ref: previous readme challenge I did)
 const writeToFile = (html) => {
     const outputPath = path.resolve(__dirname, 'output', 'team.html');
     fs.writeFile(outputPath, html, (err) => {
        if (err) throw err;
-       console.log('The file has been saved!');
+       console.log('You have successfully saved your file!');
     });
    };
    
    // Function to initialize the application
    function init() {
     let teamMembers = [];
+
+    const addTeamMember = () => {
+        inquirer
+          .prompt([
+            {
+              type: 'list',
+              name: 'addMember',
+              message: 'Please enter your team manager name?',
+              choices: ['Engineer', 'Intern', 'Team Manager'],
+            },
+          ])
+          .then((answers) => {
+            if (answers.addMember === 'Engineer') {
+              addEngineer();
+            } else if (answers.addMember === 'Intern') {
+              addIntern();
+            } else {
+              finishBuilding();
+            }
+          });
+     }};
+     const addEngineer = () => {
+        inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'engineerName',
+              message: "Engineer's Name:",
+            },
+            {
+              type: 'input',
+              name: 'engineerId',
+              message: "Engineer's ID:",
+            },
+            {
+              type: 'input',
+              name: 'engineerEmail',
+              message: "Engineer's Email:",
+            },
+            {
+              type: 'input',
+              name: 'engineerGithub',
+              message: "Engineer's GitHub username:",
+            },
+          ])
+          .then((answers) => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            addTeamMember();
+          });
+     };
