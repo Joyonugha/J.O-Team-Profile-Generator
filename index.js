1f51768bb666de20d11225172b50b55799a3ea13
Joy Onugha <joyonugha@gmail.com>
2024-03-03 16:21:44 +0000

    added the intern's function to index.js

--- index.js
+++ index.js
@@ -114,3 +114,21 @@ const writeToFile = (html) => {
             addTeamMember();
           });
      };
+     const addIntern = () => {
+        inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'internName',
+              message: "Intern's Name:",
+            },
+            {
+              type: 'input',
+              name: 'internId',
+              message: "Intern's ID:",
+            },
+            {
+              type: 'input',
+              name: 'internEmail',
+              message: "Intern's Email:",
+             
